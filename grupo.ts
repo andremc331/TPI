df56b257c1c634ae087c2a76d86eb652b188025f
andremc331 <andremc331@gmail.com>
2024-11-13 15:26:23 -0300

    first

--- grupo.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import controller from "../controllers/GrupoController";
-const routes = Router();
--
-routes.post('/', controller.create);
-routes.get('/', controller.list);
-routes.delete('/', controller.delete);
-routes.put('/', controller.update);
-export default routes;
